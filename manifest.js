13c51a781bc9ba238e2f40b439bab2c5a1d2c9f5
nsankaran <nsankaran@ebay.com>
2012-07-25 11:28:22 -0700

    added manifest

---
+++ manifest.js
@@ -0,0 +1,12 @@
+{
+	"name": "Swap JS",
+	"version": "0.1",
+	"manifest_version": 1,
+	"description": "Debugger that can swap minified Javascript with human readable Javascript.",
+	"browser_action": {
+		"default_icon": "icon.png"
+	},
+	"permissions": [
+		"http://api.flickr.com/"
+		]
+}
