5e793ee6144f2b1fc6cf9f7407c023869e8bbae4
nsankaran <nsankaran@ebay.com>
2012-07-31 15:58:01 -0700

    saving wokr

--- src/chrome/popup.js
+++ src/chrome/popup.js
@@ -4,39 +4,61 @@
 var port=null;
 var tabId=null;
 
+var getScriptsCmd = { 'command' : 'get_scripts' };
+
+/**
+   Setup : 
+   [1] get the current tab, 
+   [2] create a port, 
+   [3] add port-listeners and 
+   [4] associate an event with the 'div's.
+ */
 chrome.windows.getCurrent(function(win) { 
+
+	// find the tab.id of current tab
         chrome.tabs.query( {'windowId': win.id, 'active': true}, function(tabArray){ 
             if( tabArray instanceof Array ){
                 alert("tabs " + tabArray.length  + " " + tabArray[0].id );
                 tabId = tabArray[0].id;
             }
+
+	    // create a port
+     	    if( port == null){
+		var name = { "name" : "swap"};
+		alert("connecting to tab : " + tabId);
+		port = chrome.tabs.connect( tabId, name);
+		port.postMessage( getScriptsCmd);
+	    
+		// handle incoming messages
+		port.onMessage.addListener(function(msg){
+		    if( msg != null ){
+			    alert(msg);
+		    }
+		});
+     	    }
+
+
+	    // add an 'click' event listener for 'div' elements
+	    document.addEventListener( 'DOMContentLoaded', function(){
+		var divs = document.getElementsByTagName('div');
+		for( var i=0; i< divs.length ; i++ ){
+		   divs[i].addEventListener('click', click );
+		}
+	    });
         }); 
 });
 
-chrome.tabs.getSelected( null, function(tab){
-        alert( "tab [" + tab.id + "]" );
-     if( port == null){
-        var name = { "name" : "swap"};
-        port = chrome.tabs.connect( tab.id, name);
-     }
-});
-
+/* function to send a message to the 'content' script port */
 function click( e ){
 	chrome.tabs.getSelected(null, function( tab ){
 		var cmd = { "command" : "get_scripts" };
-        if( port != null ){
-            port.postMessage( cmd );
-        }else{
-            alert("connection doesn't exist");
-        }
+		if( port != null ){
+			port.postMessage( cmd );
+		}else{
+			alert("connection doesn't exist");
+		}
 	});
 }
 
-document.addEventListener( 'DOMContentLoaded', function(){
-	var divs = document.getElementsByTagName('div');
-	for( var i=0; i< divs.length ; i++ ){
-		divs[i].addEventListener('click', click );
-	}
-});
 
 
--- src/chrome/swap.js
+++ src/chrome/swap.js
@@ -13,7 +13,7 @@ var port = chrome.extension.connect({"name":"swap"});
 
 chrome.extension.onConnect.addListener(function(port){
         port.onMessage.addListener(function( msg ){
-            alert("CS [" + msg + "]" );
+            alert("CS [" + JSON.stringify(msg) + "]" );
         });
 });
 
