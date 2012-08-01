af7fec0e28e893854bd8a1313aeee73ac2ace3e2
Nambi Sankaran <snambi@gmail.com>
2012-08-01 10:44:47 -0700

    added content security

--- src/chrome/popup.js
+++ src/chrome/popup.js
@@ -1,10 +1,9 @@
 
 
-// connect to the content script
+// global variables
 var port=null;
 var tabId=null;
-
-var getScriptsCmd = { 'command' : 'get_scripts' };
+var cmd_get_scripts = { 'command' : 'get_scripts' };
 
 /**
    Setup : 
@@ -13,40 +12,43 @@ var getScriptsCmd = { 'command' : 'get_scripts' };
    [3] add port-listeners and 
    [4] associate an event with the 'div's.
  */
-chrome.windows.getCurrent(function(win) { 
+function init(){
 
-	// find the tab.id of current tab
-        chrome.tabs.query( {'windowId': win.id, 'active': true}, function(tabArray){ 
+    // find the current tab
+    chrome.windows.getCurrent(function(win) { 
+
+	    // find the tab.id of current tab
+        chrome.tabs.query( {'windowId': win.id, 'active': true}, function(tabArray){
             if( tabArray instanceof Array ){
                 alert("tabs " + tabArray.length  + " " + tabArray[0].id );
                 tabId = tabArray[0].id;
             }
 
-	    // create a port
+	        // create a port
      	    if( port == null){
-		var name = { "name" : "swap"};
-		alert("connecting to tab : " + tabId);
-		port = chrome.tabs.connect( tabId, name);
-		port.postMessage( getScriptsCmd);
+		        var name = { "name" : "swap"};
+		        alert("connecting to tab : " + tabId);
+		        port = chrome.tabs.connect( tabId, name);
+		        port.postMessage( cmd_get_scripts );
 	    
-		// handle incoming messages
-		port.onMessage.addListener(function(msg){
-		    if( msg != null ){
-			    alert(msg);
-		    }
-		});
+		        // handle incoming messages
+		        port.onMessage.addListener(function(msg){
+		            if( msg != null ){
+			            alert(msg);
+		            }
+		        });
      	    }
 
-
-	    // add an 'click' event listener for 'div' elements
-	    document.addEventListener( 'DOMContentLoaded', function(){
-		var divs = document.getElementsByTagName('div');
-		for( var i=0; i< divs.length ; i++ ){
-		   divs[i].addEventListener('click', click );
-		}
-	    });
+	        // add an 'click' event listener for 'div' elements
+	        document.addEventListener( 'DOMContentLoaded', function(){
+		        var divs = document.getElementsByTagName('div');
+		        for( var i=0; i< divs.length ; i++ ){
+		            divs[i].addEventListener('click', click );
+		        }
+	        });
         }); 
-});
+    });
+}
 
 /* function to send a message to the 'content' script port */
 function click( e ){
@@ -59,6 +61,5 @@ function click( e ){
 		}
 	});
 }
-
 
 
